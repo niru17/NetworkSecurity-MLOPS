4d305aa60c467cfefc4e6c92a34a4b9eda83b760
Niranjana Subramanian <subramanianniranjana@gmail.com>
2025-09-23 13:27:15 -0500

    Model Trainer Implementation

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 
 import sys
 
@@ -29,5 +30,13 @@ if __name__=="__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+        
+        logging.info("Model Trainer Started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(data_transformation_artifact,model_trainer_config)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        
+        logging.info("Model Training artifact ended")
+
     except Exception as e:
         raise NetworkSecurityException(e,sys)
